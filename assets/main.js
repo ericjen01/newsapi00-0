daf96d5616a488657fa6088c5abb56f1529394ec
ericjen01 <eric.jen01@gmail.com>
2022-06-15 02:43:02 -0700

    make it better

--- assets/main.js
+++ assets/main.js
@@ -4,5 +4,23 @@ let newsList = document.querySelector(".newsList");
 fetch(url)
 	.then((res) => res.json())
 	.then((data) => {
-		console.log(data);
+		data.forEach((article) => {
+			let title = article.title;
+			let img = article.img;
+			let url = article.url;
+			let label = article.lable;
+			let date = article.date;
+
+			let li = document.createElement("li");
+
+			li.innerHTML = `
+				<div class="imgWrapper">
+					<img class="newsImg" src="${img}" alt="">
+				</div>
+				<p class="label">${label}  |  ${date}</p>
+				<h1 class="title">${title}</h1>
+				<a class = "read" href="${url}">Read More</a>
+			`;
+			newsList.appendChild(li);
+		});
 	});
